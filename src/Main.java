cd0e742d3a95b5231ab9282daa7663b4d1c7fbf7
Mathias Lindblom <mathias.lindblom@trustly.com>
2013-12-03 17:55:36 +0100

    Naive Greedy Algorithm working

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author mathiaslindblom
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in;
+
+        //        in = new BufferedReader(new FileReader("some_file.txt"));
+        in = new BufferedReader(new InputStreamReader(System.in));
+
+        int points = Integer.parseInt(in.readLine());
+        float[] inputCoords = new float[points*2];
+        String[] pointCoord;
+        for (int i = 0; i < points; i++) {
+            pointCoord = in.readLine().split(" ");
+            inputCoords[i * 2] = Float.parseFloat(pointCoord[0]);
+            inputCoords[i * 2 + 1] = Float.parseFloat(pointCoord[1]);
+        }
+
+        NaiveGreedy naiveGreedy = new NaiveGreedy(inputCoords);
+        int[] bestPath = naiveGreedy.findPath();
+
+        for(int point: bestPath){
+            System.out.println(point);
+        }
+    }
+}
---
+++ src/NaiveGreedy.java
@@ -0,0 +1,40 @@
+/**
+ * @author mathiaslindblom
+ */
+public class NaiveGreedy {
+    float[] pointsCoordinates;
+    int[]   pathTaken;
+    int     numberOfPoints;
+
+    public NaiveGreedy(float[] inputCoords) {
+        numberOfPoints = inputCoords.length / 2;
+        pointsCoordinates = inputCoords;
+        pathTaken = new int[numberOfPoints];
+    }
+
+    public int[] findPath() {
+        pathTaken[0] = 0;
+        boolean[] usedPoints = new boolean[numberOfPoints];
+        usedPoints[0] = true;
+        for (int i = 1; i < numberOfPoints; i++) {
+            int best = -1;
+            for (int j = 0; j < numberOfPoints; j++) {
+                if (!usedPoints[j] && (best == -1 || dist(pathTaken[i - 1], j) < dist(pathTaken[i - 1], best))) {
+                    best = j;
+                }
+            }
+            pathTaken[i] = best;
+            usedPoints[best] = true;
+        }
+        return pathTaken;
+    }
+
+    private int dist(int p1, int p2) {
+        float x1 = pointsCoordinates[p1 * 2];
+        float y1 = pointsCoordinates[p1 * 2 + 1];
+        float x2 = pointsCoordinates[p2 * 2];
+        float y2 = pointsCoordinates[p2 * 2 + 1];
+
+        return (int) (Math.sqrt((x1 - x2) * (x1 - x2) + (y1-y2) * (y1-y2)) + 0.5);
+    }
+}
